24cad34792e1e1311b68bec44839bb975a52f962
Cath Riddoch <cathriddoch@gmail.com>
2021-02-18 18:11:43 +0000

    Exercise 2 reading in data and writing it out

--- R/part03.R
+++ R/part03.R
@@ -1,9 +1,16 @@
 library(tidyverse)
 
-#Practice using read_csv()
-re_data <- read_csv("data/reg_exp_test.csv")
-re_data
-
-#Practice using read_tsv()
-braemar_data <- read_tsv("data/braemar_raw.tsv")
-braemar_data
+#Exercise 1
+  #Practice using read_csv()
+  re_data <- read_csv("data/reg_exp_test.csv")
+  re_data
+  
+  #Practice using read_tsv()
+  braemar_data <- read_tsv("data/braemar_raw.tsv")
+  braemar_data
+  
+#Exercise 2
+  #write a dataset to a file and reading it back in again
+  #I don't have excel- lets see if numbers will work
+  
+  
