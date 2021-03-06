0e49e533e737e1a3f7ff8db94b811aabafeb5953
Cath Riddoch <cathriddoch@gmail.com>
2021-03-06 18:53:34 +0000

    working but a bit hacky

--- R/read_and_clean_weather_data.R
+++ R/read_and_clean_weather_data.R
@@ -2,10 +2,11 @@
 #Rewrite the reading in of the dataset and cleaning into a function that takes a filepath
 library(tidyverse)
 
+the_data = df[FALSE,]
 #function to read in the data
 read_in_data = function(path_to_file){
-  the_data <- read_tsv(path_to_file)
-  return(the_data)
+  the_data <<- read_tsv(path_to_file)
+  return()
 }
 
 #function to clean the data
@@ -14,20 +15,20 @@ clean_data = function(dataset){
   the_clean_data <- mutate(dataset, across(where(is.character),
                                              str_remove_all, pattern = "\\*"))%>%
     
-    #remove dashes
-    mutate(across(where(is.character), str_replace,
+  #remove dashes
+  mutate(across(where(is.character), str_replace,
                   pattern = "---",
                   replacement = NA_character_))%>%
     
-    #remove spaces
-    mutate(across(where(is.character),str_remove_all,
+  #remove spaces
+  mutate(across(where(is.character),str_remove_all,
                   pattern = " "))
   
   #remove last column
   the_clean_data <-select( the_clean_data, yyyy:`sun (hours)`)%>%
     
-    #change chr datatype columns to double
-    mutate(across(where(is.character), as.numeric))
+  #change chr datatype columns to double
+  mutate(across(where(is.character), as.numeric))
 }
 
 #testing
